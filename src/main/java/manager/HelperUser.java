c8f4fb08b0ebdfb524f600f27647e523350637be
jonathan5765 <jonathan5765@gmail.com>
2023-03-17 23:44:37 +0200

    Test Login

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,10 +1,39 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperUser extends HelperBase{
 
     public HelperUser(WebDriver wd) {
+
         super(wd);
+    }
+
+    public void openLoginRegistrationForm(){
+//        WebElement loginTab = wd.findElement(By.xpath("//a[text()='LOGIN']"));
+//        //css.Selector: "a[href]='/login']
+//        loginTab.click();
+        click(By.xpath("//a[text()='LOGIN']"));
+
+    }
+    public void fillLoginRegistrationForm(String email, String password){
+//        WebElement emailInput = wd.findElement(By.name("email"));
+//        emailInput.click();
+//        emailInput.clear();
+//        emailInput.sendKeys(email);
+        type(By.name("email"),email);
+        type(By.xpath("//input[last()]"),password);
+
+    }
+    public void submitLogin(){
+        click(By.xpath("//button[text()='Login']"));
+    }
+    public boolean isLogged(){
+        return isElementPresent(By.xpath("//button[text()='Sign Out']"));
+    }
+    public void logout(){
+        click(By.xpath("//button[text()='Sign Out']"));
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,37 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
+@BeforeMethod
+public void preCondition(){
+    //if SignOut present ---> logout
+    if(app.getHelperUser().isLogged()){
+        app.getHelperUser().logout();
+    }
+
+}
+    @Test
+    public void loginSuccess(){
+       app.getHelperUser().openLoginRegistrationForm();
+       app.getHelperUser().fillLoginRegistrationForm("tirex@gmail.com", "Rr12345$");
+       app.getHelperUser().submitLogin();
+       //time--->signOut
+
+       //Assert
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+    }
 
     @Test
-    public void test(){
+    public void loginSuccessModel(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("tirex@gmail.com", "Rr12345$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
 
     }
 }
